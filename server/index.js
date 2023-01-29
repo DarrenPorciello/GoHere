4678156675f2f48e4cba18263b6e4a1b0fa5888f
DarrenPorciello <darren.porciello1@gmail.com>
2023-01-29 15:30:05 -0500

    Full project
    The full project I created at U of T Hacks with my team. The Beta, ready for new additional modifications.

---
+++ server/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Basic express setup:
+
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = require("./lib/in-memory-db");
+
+const DataHelpers = require("./lib/data-helpers.js")(db);
+
+const eventsRoutes = require("./routes/events")(DataHelpers);
+
+app.use("/events", eventsRoutes);
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
